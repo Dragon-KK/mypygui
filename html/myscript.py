2f61f9dde64e1f7d96365210ac0abc7422315b7d
Dragon-KK <61528852+Dragon-KK@users.noreply.github.com>
2022-08-30 11:46:26 +0530

    tested behaviour of inline spans

--- html/myscript.py
+++ html/myscript.py
@@ -54,11 +54,13 @@ def on_click(e):
     #     console.debug('Changes have been made to dom')
 
 elems2 = document.get_elements_by_tag_name('h1')
+elems3 = document.get_elements_by_class_name('myspan')
 def release(*args):
     global elems, xs, elems2
     xs = None
     elems = None
     elems2 = None
+    elems3 = None
 page_closed.then(release)
 
 # xs.event_emitter.subscribe(Event.Types.key_press, lambda e:print(e.info._e.char))
@@ -89,9 +91,15 @@ def handle(elem):
 def handle2(elem):
     elem.event_emitter.subscribe(Event.Types.click, lambda e:elem.remove())
     elem.event_emitter.subscribe(Event.Types.key_press, lambda e:abc(elem, elem.children[0], e.info._e.char))
+
+def handle3(elem):
+    elem.event_emitter.subscribe(Event.Types.key_press, lambda e:abc(elem, elem, e.info._e.char))
+
 for elem in elems:
     handle(elem)
 for elem in elems2:
     handle2(elem)
+for elem in elems3:
+    handle3(elem)
 
 
